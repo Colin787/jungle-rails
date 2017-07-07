ec5b0059373c226a20ed68952b1d22e52bf47999
Colin787 <colinpark4@gmail.com>
2017-07-07 01:19:16 +0000

    added routes for login, logout, create user

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,21 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  get '/cool' => 'gif#cool'
+  get '/sweet' => 'gif#sweet'
+
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
+    
+  
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
